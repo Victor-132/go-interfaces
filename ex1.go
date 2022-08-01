fe4060f6e7cc4b4ed7befc73f3d99ae1dae8005f
Victor Matheus Tibirica Batata <victorbatata@itstransdata.com>
2022-08-01 11:14:54 -0300

    First example of interfaces and exercise of the course

---
+++ ex1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func main() {
+	s := square{sideLength: 4.62}
+	t := triangle{height: 5.25, base: 2.5}
+
+	printArea(s)
+	printArea(t)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println("The area is:", s.getArea())
+}
